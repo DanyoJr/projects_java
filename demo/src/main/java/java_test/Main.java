41615359a48ae7028c415583cee27f9681a74017
Daniel Yoneshige Junior <danielyoneshigejunior@MacBook-Air-de-Daniel.local>
2024-08-17 23:44:54 -0300

    personal project in java,most of it will be college challenges

---
+++ demo/src/main/java/java_test/Main.java
@@ -0,0 +1,25 @@
+package java_test;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner (System. in);
+
+        int vet[] = new int [5];
+
+        System.out.println("Digite 5 valores para o vetor");
+
+        for(int x=0; x<5; x++){ int valor = ler.nextInt();
+
+            if(valor % 2 == 0) {
+                vet [x] = valor;
+            }
+        }
+
+        for(int x=0; x<5; x++){
+        System.out.print(vet[x]+"\t");}
+
+    }
+}
